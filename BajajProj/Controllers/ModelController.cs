cb8b518c487336b6b3a426318132b33895b58d1f
prasadmaduranga <hprasadmaduranga@gmail.com>
2016-06-15 08:34:28 +0530

    second commit

---
+++ BajajProj/Controllers/ModelController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KendoUIApp4.Controllers
+{
+    public class ModelController : Controller
+    {
+        // GET: Model
+        public ActionResult Index()
+        {
+
+
+            return View();
+        }
+        public ActionResult SingleItem()
+        {
+
+
+            return View();
+        }
+    }
+}
---
+++ BajajProj/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BajajProj.MvcApplication" Language="C#" %>
